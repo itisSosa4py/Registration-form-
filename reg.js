f4773c2a7374bbdb607c2667a1a5da1edf7f4cd9
Pratik <142621910+itisSosa4py@users.noreply.github.com>
2026-03-15 10:51:40 +0545

    Add files via upload
    registration form mit html und css

---
+++ reg.js
@@ -0,0 +1,23 @@
+    
+function login(a) {
+    console.log(a)
+}
+
+function submit() {
+    console.log("ram")
+    const name = document.getElementById("name").value
+    const Surname = document.getElementById("Surname").value
+    const Age = document.getElementById("Age").value
+    const Gender = document.getElementById("Gender").value
+    const DOB = document.getElementById("DOB").value
+    const Email = document.getElementById("E-mail").value
+    const Website = document.getElementById("Website").value
+    console.log(name.trim())
+    console.log(Surname.trim())
+    console.log(Age.trim())
+    console.log (Gender.trim())
+    console.log  (DOB.trim()) 
+    console.log(Email.trim())
+    console.log (Website.trim())
+    
+}
